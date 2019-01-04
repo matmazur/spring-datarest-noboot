0b4048460b40d6e4c2ab1cf33c12b80b408e2968
Bingo Bobo <matmazur90@gmail.com>
2019-01-04 20:42:18 +0100

    added packages, added class City with builder

---
+++ src/main/java/model/City.java
@@ -0,0 +1,95 @@
+package model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class City {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @Column(name = "id_city")
+    private Long id;
+    private String name;
+    private Long population;
+
+    public City(String name, Long population) {
+        this.name = name;
+        this.population = population;
+    }
+
+    public City() {
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", population=" + population +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        City city = (City) o;
+        return Objects.equals(id, city.id) &&
+                Objects.equals(name, city.name) &&
+                Objects.equals(population, city.population);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, population);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Long population) {
+        this.population = population;
+    }
+
+    public static class Builder {
+
+        private String name;
+        private Long population;
+
+        public Builder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder withPopulation(Long population) {
+            this.population = population;
+            return this;
+        }
+
+        public City build() {
+            return new City(name, population);
+        }
+    }
+}
